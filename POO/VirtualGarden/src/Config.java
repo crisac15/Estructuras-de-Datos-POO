c95ed9cead99b5f9ae61c9d6c84be1e6524d67f5
crisac15 <105962786+crisac15@users.noreply.github.com>
2022-10-10 23:53:16 -0600

    Delete POO/VirtualGarden directory

--- POO/VirtualGarden/src/Config.java
+++
@@ -1,9 +0,0 @@
-
-public class Config {
-	public final static int ANNO = 365;
-	public final static int MES = 30;
-	public final static int DIA = 1;
-	public final static int ABONAR = 5;
-	public final static int REGAR = 5;
-	public final static int RESTAR_VIDA = 5;
-}
